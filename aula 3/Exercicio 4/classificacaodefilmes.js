79f9f9d0d22e16e26d60c0b4650c71a4294575e7
unknown <rafaeldasilvaenzo@gmail.com>
2023-11-13 09:44:42 -0300

    aula

---
+++ aula 3/Exercicio 4/classificacaodefilmes.js
@@ -0,0 +1,11 @@
+
+let avaliacao_do_filme1 = 7.5;
+let avaliacao_do_filme2 = 5;
+let avaliacao_do_filme3 = 9;
+let avaliacao_do_filme4 = 3.3;
+let avaliacao_do_filme5 = 8.8;
+
+if (avaliacao_do_filme1 <= 8) {
+console.log ('O filme é bom');
+}
+
---
+++ aula 6/classificacaodefilme.js
@@ -0,0 +1,54 @@
+let avaliacao_do_filme1 = 7.5;
+let avaliacao_do_filme2 = 5.6;
+let avaliacao_do_filme3 = 9;
+let avaliacao_do_filme4 = 3.3;
+let avaliacao_do_filme5 = 8.8;
+
+if (avaliacao_do_filme1 < 8) {
+
+    console.log ('O filme é bom');
+
+} else if (avaliacao_do_filme1 >= 7) {
+
+    console.log ("O filme é bom");
+}
+
+
+if (avaliacao_do_filme2 < 7) {
+
+    console.log ('O filme é mediano');
+
+} else if (avaliacao_do_filme2 >= 5) {
+
+    console.log ("O filme é mediano");
+}
+
+
+if (avaliacao_do_filme3 < 10) {
+
+    console.log ('O filme é exelente');
+
+} else if (avaliacao_do_filme3 >= 9) {
+
+    console.log ("O filme é exelente"); 
+}
+
+
+if (avaliacao_do_filme4 < 5 ) {
+
+    console.log ('O filme é ruim');
+
+} else if (avaliacao_do_filme4 >= 0) {
+
+    console.log ("O filme é ruim"); 
+}
+
+
+if (avaliacao_do_filme5 < 9 ) {
+
+    console.log ('O filme é muito bom');
+
+} else if (avaliacao_do_filme5 >= 8) {
+
+    console.log ("O filme é muito bom"); 
+}
